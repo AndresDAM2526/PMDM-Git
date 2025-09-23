a1c4af02f776d4f0bf8ef74ed08e1517cba74f4c
AndresDAM2526 <williaman.corgar@educa.jcyl.es>
2025-09-23 11:09:26 +0200

    Empezando anagrama

--- Ejercicios2/app/src/main/java/com/example/ejercicios/MainActivity.kt
+++ Ejercicios2/app/src/main/java/com/example/ejercicios/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        inicializarComponentes()
     }
 
     private fun inicializarComponentes(){
