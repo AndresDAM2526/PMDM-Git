677a4c40dd2621a5860b80810f159b3b1a80ec46
andrescg01 <andrescg2001@gmail.com>
2025-10-07 19:19:59 +0200

    Se replica el ejercicio en clase

--- a/app/src/main/java/com/example/a/Conducible.kt
+++
@@ -1,6 +0,0 @@
-package com.example.a
-
-interface Conducible {
-        abstract fun arrancar(): String
-        abstract fun detener(): String
-}
--- a/app/src/main/java/com/example/a/Vehiculo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.a
-
-abstract class Vehiculo(var marca:String,var modelo:String,var anio:Int,var velocidadMaxima:Int):Conducible{
-    override fun arrancar()="El vehículo $marca ha arrancado"
-
-    override fun detener()="El vehículo $marca se ha detenido"
-    abstract fun acelerar(): String
-}
