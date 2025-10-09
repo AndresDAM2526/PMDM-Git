9f969a287be43b6ae520becb6820803c548e2a78
AndresDAM2526 <williaman.corgar@educa.jcyl.es>
2025-10-09 09:35:33 +0200

    Se termina el ejecicio

--- EjemploPOO2/app/src/main/java/com/example/ejemplopoo/MainActivity.kt
+++ EjemploPOO2/app/src/main/java/com/example/ejemplopoo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.ejemplopoo
 
+import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -42,24 +46,37 @@ class MainActivity : AppCompatActivity() {
         miAdaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         miSpinner.adapter=miAdaptador
         findViewById<Button>(R.id.btSimular).setOnClickListener {
-            when(miSpinner.selectedItem){
-                "Camion"->{
-                    var camion=Camion("1","2",2020,120,1200.0)
-                    simularConduccion(camion)
-                }
-                "Motocicleta"->{
-                    var moto= Motocicleta("2","34",2025,120,)
-                    Toast.makeText(this,moto.arrancar().toString(), Toast.LENGTH_LONG).show()
-                    Toast.makeText(this,moto.acelerar().toString(), Toast.LENGTH_LONG).show()
-                    Toast.makeText(this,moto.detener().toString(), Toast.LENGTH_LONG).show()
-                }
-                "Auto"->{
-                    var auto= Auto("3","43",2025,120,5)
-                    Toast.makeText(this,auto.arrancar().toString(), Toast.LENGTH_LONG).show()
-                    Toast.makeText(this,auto.acelerar().toString(), Toast.LENGTH_LONG).show()
-                    Toast.makeText(this,auto.detener().toString(), Toast.LENGTH_LONG).show()
-                }
+            var miVehiculo: Vehiculo?=null
+            when(miSpinner.selectedItem.toString()){
+                "Camion"->//Instancio el objeto de la clase camión
+                    miVehiculo=Camion("1","2",2020,120,1200.0)
+                "Motocicleta"-> //Instancio el objeto de la clase motocicleta
+                    miVehiculo= Motocicleta("Suzuki","530",2025,120,)
+
+                "Auto" -> //Instancio el objeto de la clave vehiculo
+                     miVehiculo= Auto("3","43",2025,120,5)
+
+
             }
+            //Mostrar em un AlertDialog todos los mensajes que se
+            //generan con la invocación de los métocoos arrancar, acelerar y
+            //detener
+
+            var builderDialog= AlertDialog.Builder(this)
+            builderDialog.apply {
+                setTitle(miSpinner.selectedItem.toString())
+                setMessage("${miVehiculo?.arrancar()} \n${miVehiculo?.acelerar()}\n${miVehiculo?.detener()}")
+                setPositiveButton("Aceptar",{
+                    d,which ->
+                    if(which== DialogInterface.BUTTON_POSITIVE) {
+                        d.dismiss()
+                    }
+                })
+            }
+            //Muestro el AlertDialog
+            builderDialog.create().show()
         }
+
+
     }
 }
